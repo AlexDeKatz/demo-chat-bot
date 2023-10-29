8140aa725698d6665c8cc7e377d8f478b6b8e552
Likhith Kolayari <kplikhith@gmail.com>
2023-10-29 17:10:38 +0400

    added cors

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
 import configViewEngine from "./configs/viewEngine";
 import initWebRoutes from "./routes/web";
 
 const app = express();
 
+app.use(cors());
+
 //config server to pass data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/favicon.ico", (req, res) => {});
 
 //config view engine
 configViewEngine(app);
